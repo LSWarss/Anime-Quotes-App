42c19d46b7026f745f41f378ed2f720852b54b0d
LSWarss <ls.warss98@gmail.com>
2022-03-04 07:33:20 +0100

    Initial Commit

---
+++ Shared/Anime_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anime_QuotesApp.swift
+//  Shared
+//
+//  Created by Łukasz Stachnik on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Anime_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
